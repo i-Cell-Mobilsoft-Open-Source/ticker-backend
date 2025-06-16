e6d83b29f04b57ce318be4805181be7871e4eab4
Cserhati Tamas <cserhatit@gmail.com>
2025-06-16 17:03:32 +0200

    rebase

--- ticker-common/ticker-common-system-rest/src/main/java/hu/icellmobilsoft/ticker/common/rest/SystemRest.java
+++ ticker-common/ticker-common-system-rest/src/main/java/hu/icellmobilsoft/ticker/common/rest/SystemRest.java
@@ -19,19 +19,9 @@
  */
 package hu.icellmobilsoft.ticker.common.rest;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-
 import jakarta.enterprise.inject.Model;
-import jakarta.inject.Inject;
 
-import hu.icellmobilsoft.coffee.cdi.logger.AppLogger;
-import hu.icellmobilsoft.coffee.cdi.logger.ThisLogger;
-import hu.icellmobilsoft.coffee.rest.rest.BaseRestService;
-import hu.icellmobilsoft.coffee.se.api.exception.BaseException;
-import hu.icellmobilsoft.ticker.common.api.system.ISystemRest;
+import hu.icellmobilsfoft.coffee.module.quarkus.rest.system.AbstractQuarkusSystemRest;
 
 /**
  * System rest endpoint implementations
@@ -40,28 +30,5 @@ import hu.icellmobilsoft.ticker.common.api.system.ISystemRest;
  * @since 0.3.0
  */
 @Model
-public class SystemRest extends BaseRestService implements ISystemRest {
-
-    @Inject
-    @ThisLogger
-    AppLogger log;
-
-    @Override
-    public String versionInfo() throws BaseException {
-        try {
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
-            StringBuilder version = new StringBuilder();
-            if (inputStream != null) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-                String line;
-                while ((line = br.readLine()) != null) {
-                    version.append(line);
-                    version.append("\n");
-                }
-            }
-            return version.toString();
-        } catch (Exception e) {
-            throw baseExceptionWithLogging(e);
-        }
-    }
+public class SystemRest extends  AbstractQuarkusSystemRest {
 }
